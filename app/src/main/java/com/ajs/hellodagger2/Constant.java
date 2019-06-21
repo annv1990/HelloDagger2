fde2d34d811a5dedacf4311142a99f9abfab57a1
AnNV <an.nguyen@co-well.vn>
2019-06-21 10:59:22 +0700

    - edit

---
+++ app/src/main/java/com/ajs/hellodagger2/Constant.java
@@ -0,0 +1,5 @@
+package com.ajs.hellodagger2;
+
+public class Constant {
+    public static final String TAG = "com.ajs.hellodagger2";
+}
--- app/src/main/java/com/ajs/hellodagger2/MainActivity.java
+++ app/src/main/java/com/ajs/hellodagger2/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.ajs.hellodagger2.di.component.ActivityComponent;
@@ -32,10 +34,21 @@ public class MainActivity extends AppCompatActivity {
 
         mTvUserInfo = (TextView) findViewById(R.id.tv_user_info);
         mTvAccessToken = (TextView) findViewById(R.id.tv_access_token);
+
+        Button btn = findViewById(R.id.btnTest);
+        btn.setCompoundDrawablesWithIntrinsicBounds(R.drawable.iconfinder, 0, 0, 0);
+        createUser();
+        getUser();
+        mDataManager.saveAccessToken("ASDR12443JFDJF43543J543H3K543");
+
+        String token = mDataManager.getAccessToken();
+        if (token != null) {
+            mTvAccessToken.setText(token);
+        }
     }
 
     public ActivityComponent getActivityComponent() {
-        if(activityComponent == null) {
+        if (activityComponent == null) {
             activityComponent = DaggerActivityComponent.builder().activityModule(new ActivityModule(this))
                     .applicationComponent(HelloDagger2Application.get(this).getComponent()).build();
         }
@@ -46,26 +59,31 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onPostCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
         super.onPostCreate(savedInstanceState, persistentState);
-        createUser();
+        Log.d(Constant.TAG, "onPostCreate");
+        /*createUser();
         getUser();
         mDataManager.saveAccessToken("ASDR12443JFDJF43543J543H3K543");
 
         String token = mDataManager.getAccessToken();
         if(token != null){
             mTvAccessToken.setText(token);
+        }*/
+    }
+
+    private void createUser() {
+        try {
+            mDataManager.createUser(new User("Ali", "1367, Gurgaon, Haryana, India"));
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
-    private void createUser(){
-        try {
-            mDataManager.createUser(new User("Ali", "1367, Gurgaon, Haryana, India"));
-        }catch (Exception e){e.printStackTrace();}
-    }
-
-    private void getUser(){
+    private void getUser() {
         try {
             User user = mDataManager.getUser(1L);
             mTvUserInfo.setText(user.toString());
-        }catch (Exception e){e.printStackTrace();}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
